5efe36e8dc4d95f0c97a617c0b88207a6e835035
ReBuilders101 <rebuilders101@gmail.com>
2018-08-31 19:07:55 +0200

    Added javadoc to scene classes

--- src/lb/simplebase/scene/SceneContainer.java
+++ src/lb/simplebase/scene/SceneContainer.java
@@ -2,25 +2,76 @@ package lb.simplebase.scene;
 
 import java.awt.Graphics2D;
 
+/**
+ * @version 1.1
+ * @author LB
+ * This scene acts as as a container for other scenes. The subscenes can be resized and positioned in the frame.
+ * All subscenes will receive calls to their {@link #tick()} and {@link #draw(Graphics2D)} method.
+ * Note that there are special methods for ticking and drawing the container itself.
+ */
 public abstract class SceneContainer extends Scene{
 	
 	private Scene[] subscenes;
 	
+	/**
+	 * Get a list of all subscenes countained in this scene
+	 * The array fields can be chabged to swap out scenes, however this is not advised and is not guaranteed to work with all implementations.
+	 * Implementations may override this method so a copy of the array is returned.
+	 * @return The subscene array.
+	 */
 	public Scene[] getSubscenes(){
 		return subscenes;
 	}
 	
+	/**
+	 * Searches for a subscene by it's name
+	 * @param name The name of the scene
+	 * @return The scene or null if no scene was found
+	 */
+	public Scene getSubscene(String name){
+		for(Scene s : subscenes){
+			if(s.getName().equals(name)) return s;
+		}
+		return null;
+	}
+	
+	/**
+	 * The draw method for the container background. This method is called before the subscenes are drawn.
+	 * @param g The graphics object to draw into
+	 */
 	public abstract void drawContainer(Graphics2D g);
+	
+	/**
+	 * The draw method for the container foreground. This method is called after the subscenes are drawn.
+	 * @param g The graphics object to draw into
+	 */
+	public abstract void drawContainerForeground(Graphics2D g);
+	
+	/**
+	 * The tick method for the container. As containers rarely do logic on their own, this method will be often empty
+	 * This method is called before the tick() - methods of the subscenes
+	 */
 	public abstract void tickContainer();
 	
+	/**
+	 * An implementation of the draw() - method that calls {@link #drawContainer(Graphics2D)}, then draws all subscenes and then
+	 * calls {@link #drawContainerForeground(Graphics2D)}. This method should not be overridden, instead the methods mentioned above may be used.
+	 * If it is overridden, the new implementation must make sure that the subscenes are drawn properly.
+	 */
 	@Override
 	public void draw(Graphics2D g){
 		drawContainer(g);
 		for(Scene s : subscenes){
 			s.draw(g);
 		}
+		drawContainerForeground(g);
 	}
 	
+	/**
+	 * An implementation of the tick() - method that calls {@link #tickContainer(Graphics2D)} and then calls tick() on all subscenes.
+	 *  This method should not be overridden, instead the method mentioned above may be used.
+	 * If it is overridden, the new implementation must make sure that the subscenes are ticked properly.
+	 */
 	@Override
 	public void tick(){
 		tickContainer();
