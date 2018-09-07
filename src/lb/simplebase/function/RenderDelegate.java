6b96e6f2ca89ccbee9f4daa2bfa4fb309f3207e7
ReBuilders101 <rebuilders101@gmail.com>
2018-09-07 17:43:37 +0200

    Added Functional Scene

---
+++ src/lb/simplebase/function/RenderDelegate.java
@@ -0,0 +1,7 @@
+package lb.simplebase.function;
+
+import java.awt.Graphics2D;
+
+public interface RenderDelegate extends TriConsumer<Graphics2D, Integer, Integer>{
+
+}
---
+++ src/lb/simplebase/function/TriConsumer.java
@@ -0,0 +1,7 @@
+package lb.simplebase.function;
+
+public interface TriConsumer<T,S,U> {
+
+	public void accept(T t, S s, U u);
+	
+}
