71675858b56f65882841a7b73c77c4db167498a7
ReBuilders101 <rebuilders101@gmail.com>
2018-09-10 18:06:12 +0200

    Finished NumberElement

--- src/lb/simplebase/math/expression/NumberElement.java
+++ src/lb/simplebase/math/expression/NumberElement.java
@@ -1,31 +1,51 @@
 package lb.simplebase.math.expression;
 
-public class NumberElement extends Number implements CalculatableElement{
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+import lb.simplebase.math.expression.render.RenderSymbol;
+import lb.simplebase.math.expression.render.SymbolRegistry;
+
+public class NumberElement implements CalculatableElement{
 
 	private static final long serialVersionUID = -1909384316766936204L;
 
+	private BigDecimal bigDec;
+	
+	public NumberElement(double number) {
+		bigDec = BigDecimal.valueOf(number);
+	}
+	
+	public NumberElement(String number) {
+		bigDec = new BigDecimal(number);
+	}
+	
+	public NumberElement(BigDecimal big) {
+		bigDec = big; //This is ok because BigInteger is immutable
+	}
+	
+	public NumberElement(BigInteger bigInt) {
+		bigDec = new BigDecimal(bigInt);
+	}
+
+	public static NumberElement valueOf(double number) {
+		return new NumberElement(number);
+	}
+	
+	public static NumberElement valueOf(BigDecimal bigDec) {
+		return new NumberElement(bigDec);
+	}
+	
+	//Implemented Methods
+	
 	@Override
-	public double doubleValue() {
-		// TODO Auto-generated method stub
-		return 0;
+	public RenderSymbol<NumberElement> getSymbol() {
+		return SymbolRegistry.getRegistry().getMapping(NumberElement.class);
 	}
 
 	@Override
-	public float floatValue() {
-		// TODO Auto-generated method stub
-		return 0;
+	public BigDecimal getValue() {
+		return bigDec;
 	}
-
-	@Override
-	public int intValue() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public long longValue() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
+	
 }
