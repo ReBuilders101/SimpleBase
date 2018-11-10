b67cbda54a7aec840751f53a6990a3e6f90cd881
ReBuilders101 <rebuilders101@gmail.com>
2018-11-10 19:25:56 +0100

    Marker Annotations

---
+++ src/lb/simplebase/net/ClientSide.java
@@ -0,0 +1,14 @@
+package lb.simplebase.net;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.CLASS;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Retention(CLASS)
+@Target({ TYPE, METHOD })
+public @interface ClientSide {
+
+}
