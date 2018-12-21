2f1f66de8ae06f876974e1bd6f625a35ed454a24
ReBuilders101 <rebuilders101@gmail.com>
2018-12-21 19:54:19 +0100

    Error handling/logging package
    There probably is a much better library for this, but I want to do most
    of this stuff by myself.

---
+++ src/lb/simplebase/error/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package contains classes and enums used in error handling, mainly with the goal to avoid
+ * 'hard' crashes and generate detailed crash reports.
+ */
+package lb.simplebase.error;
