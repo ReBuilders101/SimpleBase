1c65714224b8fab374989ebedc84fc67d5a387d6
ReBuilders101 <rebuilders101@gmail.com>
2019-01-02 16:02:09 +0100

    package-info for the net and net.localimpl packages

---
+++ src/lb/simplebase/net/localimpl/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package contains an implementation of the network code from the {@link lb.simplebase.net} package
+ * for integrated client / server applications
+ */
+package lb.simplebase.net.localimpl;
---
+++ src/lb/simplebase/net/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * This package contains abstract interfaces and classes that can be used for network communication
+ */
+package lb.simplebase.net;
