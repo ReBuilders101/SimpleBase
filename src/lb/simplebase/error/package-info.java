71db024c4bb9f1b39bebc9d505ca0113edcb9f29
ReBuilders101 <rebuilders101@gmail.com>
2019-02-07 13:56:59 +0100

    Put error package in a separate branch

--- src/lb/simplebase/error/package-info.java
+++ src/lb/simplebase/error/package-info.java
@@ -1,5 +1,7 @@
 /**
  * This package contains classes and enums used in error handling, mainly with the goal to avoid
  * 'hard' crashes and generate detailed crash reports.
+ * <p>
+ * Not contained in the master branch anymore, replaced by the log package
  */
 package lb.simplebase.error;
