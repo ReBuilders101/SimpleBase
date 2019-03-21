e2a383fb9fad8118a7b64ac1f92e0080fec363fa
ReBuilders101 <rebuilders101@gmail.com>
2019-03-21 22:20:36 +0100

    Added Function Graphs

---
+++ src/lb/simplebase/javacore/scene/FunctionGraph.java
@@ -0,0 +1,100 @@
+package lb.simplebase.javacore.scene;
+
+import java.awt.Graphics2D;
+import java.awt.Paint;
+import java.util.function.DoubleFunction;
+
+public abstract class FunctionGraph implements RangedDrawable{
+
+	private int unitStep;
+	private Paint paint;
+	private FunctionGraphStyle style;
+	
+	protected FunctionGraph(FunctionGraphStyle style, Paint paint, int unitStep) {
+		this.unitStep = unitStep;
+		this.style = style;
+		this.paint = paint;
+	}
+	
+	public static FunctionGraph fromFunction(FunctionGraphStyle style, Paint paint, int unitStep, DoubleFunction<Double> func) {
+		return new FunctionalFunctionGraph(style, paint, unitStep, func);
+	}
+	
+	public abstract double getYValue(double xValue);
+
+	@Override
+	public void draw(Graphics2D g2d, int width, int height, double minXunits, double minYunits, double maxXunits, double maxYunits) {
+		final double spanXunits = maxXunits - minXunits;
+		final double spanYunits = maxYunits - minYunits;
+		final double unitXSizePx = width  / spanXunits;
+		final double unitYSizePx = height / spanYunits;
+		
+		final double localUnitStep = unitStep <= 0 ? 1D / unitXSizePx : unitStep; //if no step set, use size of one pixel in units
+		
+		int lastPosX = 0, lastPosY = 0;
+		double startXval = minXunits;
+		if(style == FunctionGraphStyle.LINE) {
+			lastPosX = (int) ((startXval - minXunits) * unitXSizePx);
+			lastPosY = height - (int) ((getYValue(startXval) - minYunits) * unitYSizePx);
+			startXval += localUnitStep;
+		}
+		
+		g2d.setPaint(paint);
+		
+		for(double x = startXval; x <= maxXunits; x += localUnitStep) {
+			//Draw at x units
+			double y = getYValue(x);
+			int xPx = (int) ((x - minXunits) * unitXSizePx);
+			int yPx = height - (int) ((y - minYunits) * unitYSizePx); //Flip
+
+			if(style == FunctionGraphStyle.LINE) {
+				g2d.drawLine(lastPosX, lastPosY, xPx, yPx);
+				lastPosX = xPx;
+				lastPosY = yPx;
+			} else { //Draw a dot
+				g2d.fillOval(xPx-1, yPx-1, 2, 2);
+			}
+		}
+	}
+
+	public int getUnitStep() {
+		return unitStep;
+	}
+
+	public void setUnitStep(int unitStep) {
+		this.unitStep = unitStep;
+	}
+
+	public Paint getPaint() {
+		return paint;
+	}
+
+	public void setPaint(Paint paint) {
+		this.paint = paint;
+	}
+
+	public FunctionGraphStyle getStyle() {
+		return style;
+	}
+
+	public void setStyle(FunctionGraphStyle style) {
+		this.style = style;
+	}
+	
+	private static class FunctionalFunctionGraph extends FunctionGraph {
+
+		private final DoubleFunction<Double> func;
+		
+		protected FunctionalFunctionGraph(FunctionGraphStyle style, Paint paint, int unitStep, DoubleFunction<Double> func) {
+			super(style, paint, unitStep);
+			this.func = func;
+		}
+
+		@Override
+		public double getYValue(double xValue) {
+			return func.apply(xValue);
+		}
+		
+	}
+	
+}
---
+++ src/lb/simplebase/javacore/scene/FunctionGraphStyle.java
@@ -0,0 +1,7 @@
+package lb.simplebase.javacore.scene;
+
+public enum FunctionGraphStyle {
+	
+	LINE, POINTS;
+	
+}
