0caee5be3077a1157c4fb48c3b7a0ffb80a3a9eb
ReBuilders101 <rebuilders101@gmail.com>
2019-03-21 18:58:10 +0100

    Added on/off switch and more null checks to SliderController

--- src/lb/simplebase/javacore/scene/SliderController.java
+++ src/lb/simplebase/javacore/scene/SliderController.java
@@ -8,24 +8,59 @@ public class SliderController {
 	private TransitionTask currentTask;
 	
 	private boolean autoClear = false;
+	private boolean active = false;
+	
+	public SliderController(Consumer<Double> valueUpdater, TransitionTask inactiveTask) {
+		this.valueUpdater = valueUpdater;
+		this.currentTask = inactiveTask;
+		this.active = false;
+	}
 	
 	public SliderController(Consumer<Double> valueUpdater) {
-		this.valueUpdater = valueUpdater;
+		this(valueUpdater, null);
+		
 	}
 	
 	public void update() {
-		if(currentTask != null) {
+		if(currentTask != null && active) {
 			valueUpdater.accept(currentTask.getNextValue());
 			if(autoClear && currentTask.isDone()) currentTask = null;
 		}
 	}
 	
-	public void setActiveTask(TransitionTask task) {
+	public void setTaskActive(TransitionTask task) {
 		currentTask = task;
+		active = true;
 	}
 	
-	public void removeActiveTask() {
+	public void setActive(boolean value) {
+		active = value;
+	}
+	
+	public boolean isActive() {
+		return active;
+	}
+	
+	public void setTaskInactive(TransitionTask task) {
+		currentTask = task;
+		active = false;
+	}
+	
+	public void removeTask() {
 		currentTask = null;
+		active = false;
+	}
+	
+	public double getCurrentValue() {
+		return currentTask == null ? 0 : currentTask.getCurrentValue();
+	}
+	
+	public TransitionTask getCurrentTask() {
+		return currentTask;
+	}
+	
+	public void resetTask() {
+		if(currentTask != null) currentTask.reset();
 	}
 	
 	public void setRemoveTaskIfDone(boolean value) {
