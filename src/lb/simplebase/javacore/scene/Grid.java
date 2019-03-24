636dbf87d319f6e12037508848a2b604d555ed9a
ReBuilders101 <rebuilders101@gmail.com>
2019-03-24 22:19:27 +0100

    Grid class is now working with the new offset/span system

--- src/lb/simplebase/javacore/scene/Grid.java
+++ src/lb/simplebase/javacore/scene/Grid.java
@@ -7,8 +7,8 @@ import lb.simplebase.javacore.Framework;
 
 public class Grid implements RangedDrawable{
 	
-	private final GridLineStyle style;
-	private final Paint paint;
+	private GridLineStyle style;
+	private Paint paint;
 	private final double lineDistX;
 	private final double lineDistY;
 	private final int attribute;
@@ -41,109 +41,189 @@ public class Grid implements RangedDrawable{
 		return style;
 	}
 	
-	public void draw(final Graphics2D g2d, final int width, final int height, final double minXunits, final double minYunits, final double maxXunits, final double maxYunits) {
+	public void draw(final Graphics2D g2d, final int width, final int height, final double originXoffset, final double originYoffset, final double spanXunits, final double spanYunits) {
 		if(style == null) return;
-		if(minXunits >= maxXunits) throw new IllegalArgumentException("The value of minXunits may not be equal to or larger than the value of maxXunits");
-		if(minYunits >= maxYunits) throw new IllegalArgumentException("The value of minYunits may not be equal to or larger than the value of maxYunits");
+		if(spanXunits <= 0) throw new IllegalArgumentException("The span in x direction must be larger than 0");
+		if(spanYunits <= 0) throw new IllegalArgumentException("The span in y direction must be larger than 0");
 		
-		final double spanXunits = maxXunits - minXunits;
-		final double spanYunits = maxYunits - minYunits;
-		final double unitXSizePx = width  / spanXunits;
-		final double unitYSizePx = height / spanYunits;
-		final double lineDistXpx = lineDistX * unitXSizePx;
-		final double lineDistYpx = lineDistY * unitYSizePx;
+		final double unit2pixelX = width / spanXunits;
+		final double unit2pixelY = height / spanYunits;
 		
-		double offsetXpx = (minXunits % lineDistX) * unitXSizePx;
-		int lineXnum = (int) (spanXunits / lineDistX);
-		//don't draw edge lines
-		if(offsetXpx <= 0) {
-			offsetXpx += lineDistXpx;
-		}
+		//The left  / lower border of the visible area, in units
+		final int minXunits = (int) Math.ceil(-((spanXunits / 2) + originXoffset));
+		final int minYunits = (int) Math.ceil(-((spanYunits / 2) + originYoffset));
+		//The right / upper border of the visible area, in units
+		final int maxXunits = (int) Math.floor((spanXunits / 2) - originXoffset);
+		final int maxYunits = (int) Math.floor((spanYunits / 2) - originYoffset);
+		//Distance between the grid lines, in pixels
+//		final double lineDistXpx = lineDistX * unit2pixelX;
+//		final double lineDistYpx = lineDistY * unit2pixelY;
 		
-		double offsetYpx = (minYunits % lineDistY) * unitYSizePx;
-		int lineYnum = (int) (spanYunits / lineDistY);
-		if(offsetYpx <= 0) {
-			offsetYpx += lineDistYpx;
-		}
+//		double offsetXpx = (minXunits % lineDistX) * unit2pixelX;
+//		final int lineXnum = (int) (spanXunits / lineDistX);
+//		//don't draw edge lines
+//		if(offsetXpx <= 0) {
+//			offsetXpx += lineDistXpx;
+//		}
 		
-		final int localAttribute = Framework.getAttributePx(attribute);
-		
-		if(style == GridLineStyle.AXIS) {//Different from the others
-			int yAxisXpx, xAxisYpx;
-			if(minXunits >= 0) {//Y-achse am linken rand
-				yAxisXpx = 0;
-			} else if(maxXunits <= 0) { //Y-Achse am rechten rand
-				yAxisXpx = width - 1;
-			} else { //Y-Achse mittendrin, -minX in px vom linken rand
-				yAxisXpx = (int) (-minXunits * unitXSizePx);
-			}
-			
-			if(minYunits >= 0) {//Y-achse am oberen rand
-				xAxisYpx = 1;
-			} else if(maxYunits <= 0) { //Y-Achse am unteren rand
-				xAxisYpx = height;
-			} else { //Y-Achse mittendrin, -minY in px vom oberen rand
-				xAxisYpx = (int) (-minYunits * unitYSizePx);
-			}
-			//Draw axis
-			g2d.setPaint(paint);
-			g2d.drawLine(0, height - xAxisYpx, width, height - xAxisYpx);
-			g2d.drawLine(yAxisXpx, 0, yAxisXpx, height);
-			
-			//Draw lines
-			int current;
-			if(lineDistX > 0) {
-				for(int i = 0; i < lineXnum; i++) {
-					current = (int) (offsetXpx + i * lineDistXpx);
-					g2d.drawLine(current, height - xAxisYpx - localAttribute, current, height - xAxisYpx + localAttribute);
-				}
-			}
+//		double offsetYpx = (minYunits % lineDistY) * unit2pixelY;
+//		final int lineYnum = (int) (spanYunits / lineDistY);
+//		if(offsetYpx <= 0) {
+//			offsetYpx += lineDistYpx;
+//		}
 
-			if(lineDistY > 0) {
-				for(int i = 0; i < lineYnum; i++) {
-					current = height - (int) (offsetYpx + i * lineDistYpx); //Height - ... flips so y points upwards
-					g2d.drawLine(yAxisXpx - localAttribute, current, yAxisXpx + localAttribute, current);
-				}
-			}
-			return;
-		}
+		final int attributePx = Framework.getAttributePx(attribute);
 		
 		g2d.setPaint(paint);
 		
-		//Decide how to draw
-		if(style == GridLineStyle.LINE) {
-			int current;
+		switch (style) {
+		case AXIS:
+			//Axis lines
+			g2d.drawLine(0, -height / 2, 0, height / 2);
+			g2d.drawLine(-width / 2, 0, width / 2, 0);
+			
+			//Also draw the small lines
+			int current0;
+			//Draw x lines, at unitpos * unit2px
 			if(lineDistX > 0) {
-				for(int i = 0; i < lineXnum; i++) {
-					current = (int) (offsetXpx + i * lineDistXpx);
-					g2d.drawLine(current, 0, current, height);
+				for(int i = minXunits; i <= maxXunits; i++) {
+					current0 = (int) (i * unit2pixelX);
+					g2d.drawLine(current0, -attributePx, current0, attributePx);
 				}
 			}
 
 			if(lineDistY > 0) {
-				for(int i = 0; i < lineYnum; i++) {
-					current = height - (int) (offsetYpx + i * lineDistYpx); //Height - ... flips so y points upwards
-					g2d.drawLine(0, current, width, current);
+				for(int i = minYunits; i <= maxYunits; i++) {
+					current0 = (int) (i * unit2pixelY);
+					g2d.drawLine(-attributePx, current0, attributePx, current0);
 				}
 			}
-		} else if(style == GridLineStyle.DOT) {
-			//this time nested loops
-			for(int x = 0; x < lineXnum; x++) {
-				for(int y = 0; y < lineYnum; y++) {
-					g2d.fillOval((int) (offsetXpx + x * lineDistXpx) - (localAttribute / 2), height - (int) (offsetYpx + y * lineDistYpx) - (localAttribute / 2), localAttribute, localAttribute);
+			
+			return;
+		case CROSS:
+			if(lineDistX > 0 && lineDistY > 0 && attributePx > 0) {
+				for(int x = minXunits; x <= maxXunits; x++) {
+					for(int y = minYunits; y <= maxYunits; y++) {
+						g2d.drawLine((int) (x * unit2pixelX) - attributePx, (int) (y * unit2pixelY), (int) (x * unit2pixelX) + attributePx, (int) (y * unit2pixelY));
+						g2d.drawLine((int) (x * unit2pixelX), (int) (y * unit2pixelY) - attributePx, (int) (x * unit2pixelX), (int) (y * unit2pixelY) + attributePx);
+					}
 				}
 			}
-		} else if(style == GridLineStyle.CROSS) {
-			for(int x = 0; x < lineXnum; x++) {
-				for(int y = 0; y < lineYnum; y++) {
-					final int currentX = (int) (offsetXpx + x * lineDistXpx);
-					final int currentY = height - (int) (offsetYpx + y * lineDistYpx);
-					g2d.drawLine(currentX, currentY - localAttribute, currentX, currentY + localAttribute); //vertical
-					g2d.drawLine(currentX - localAttribute, currentY, currentX + localAttribute, currentY); //horizontal
+			return;
+		case DOT:
+			if(lineDistX > 0 && lineDistY > 0 && attributePx > 0) {
+				for(int x = minXunits; x <= maxXunits; x++) {
+					for(int y = minYunits; y <= maxYunits; y++) {
+						g2d.fillOval((int) (x * unit2pixelX), (int) (y * unit2pixelY), attributePx, attributePx);
+					}
 				}
 			}
+			return;
+		case LINE:
+			int current;
+			//Draw x lines, at unitpos * unit2px
+			if(lineDistX > 0) {
+				for(int i = minXunits; i <= maxXunits; i++) {
+					current = (int) (i * unit2pixelX);
+					g2d.drawLine(current, -height / 2, current, height / 2);
+				}
+			}
+
+			if(lineDistY > 0) {
+				for(int i = minYunits; i <= maxYunits; i++) {
+					current = (int) (i * unit2pixelY);
+					g2d.drawLine(-width / 2, current, width / 2, current);
+				}
+			}
+			return;
+		default:
+			return;
 		}
+//		
+//		if(style == GridLineStyle.AXIS) {//Different from the others
+//			int yAxisXpx, xAxisYpx;
+//			if(minXunits >= 0) {//Y-achse am linken rand
+//				yAxisXpx = 0;
+//			} else if(maxXunits <= 0) { //Y-Achse am rechten rand
+//				yAxisXpx = width - 1;
+//			} else { //Y-Achse mittendrin, -minX in px vom linken rand
+//				yAxisXpx = (int) (-minXunits * unitXSizePx);
+//			}
+//			
+//			if(minYunits >= 0) {//Y-achse am oberen rand
+//				xAxisYpx = 1;
+//			} else if(maxYunits <= 0) { //Y-Achse am unteren rand
+//				xAxisYpx = height;
+//			} else { //Y-Achse mittendrin, -minY in px vom oberen rand
+//				xAxisYpx = (int) (-minYunits * unitYSizePx);
+//			}
+//			//Draw axis
+//			g2d.setPaint(paint);
+//			g2d.drawLine(0, height - xAxisYpx, width, height - xAxisYpx);
+//			g2d.drawLine(yAxisXpx, 0, yAxisXpx, height);
+//			
+//			//Draw lines
+//			int current;
+//			if(lineDistX > 0) {
+//				for(int i = 0; i < lineXnum; i++) {
+//					current = (int) (offsetXpx + i * lineDistXpx);
+//					g2d.drawLine(current, height - xAxisYpx - localAttribute, current, height - xAxisYpx + localAttribute);
+//				}
+//			}
+//
+//			if(lineDistY > 0) {
+//				for(int i = 0; i < lineYnum; i++) {
+//					current = height - (int) (offsetYpx + i * lineDistYpx); //Height - ... flips so y points upwards
+//					g2d.drawLine(yAxisXpx - localAttribute, current, yAxisXpx + localAttribute, current);
+//				}
+//			}
+//			return;
+//		}
+//		
+//		g2d.setPaint(paint);
+//		
+//		//Decide how to draw
+//		if(style == GridLineStyle.LINE) {
+//			int current;
+//			if(lineDistX > 0) {
+//				for(int i = 0; i < lineXnum; i++) {
+//					current = (int) (offsetXpx + i * lineDistXpx);
+//					g2d.drawLine(current, 0, current, height);
+//				}
+//			}
+//
+//			if(lineDistY > 0) {
+//				for(int i = 0; i < lineYnum; i++) {
+//					current = height - (int) (offsetYpx + i * lineDistYpx); //Height - ... flips so y points upwards
+//					g2d.drawLine(0, current, width, current);
+//				}
+//			}
+//		} else if(style == GridLineStyle.DOT) {
+//			//this time nested loops
+//			for(int x = 0; x < lineXnum; x++) {
+//				for(int y = 0; y < lineYnum; y++) {
+//					g2d.fillOval((int) (offsetXpx + x * lineDistXpx) - (localAttribute / 2), height - (int) (offsetYpx + y * lineDistYpx) - (localAttribute / 2), localAttribute, localAttribute);
+//				}
+//			}
+//		} else if(style == GridLineStyle.CROSS) {
+//			for(int x = 0; x < lineXnum; x++) {
+//				for(int y = 0; y < lineYnum; y++) {
+//					final int currentX = (int) (offsetXpx + x * lineDistXpx);
+//					final int currentY = height - (int) (offsetYpx + y * lineDistYpx);
+//					g2d.drawLine(currentX, currentY - localAttribute, currentX, currentY + localAttribute); //vertical
+//					g2d.drawLine(currentX - localAttribute, currentY, currentX + localAttribute, currentY); //horizontal
+//				}
+//			}
+//		}
 		
+	}
+
+	public void setStyle(GridLineStyle style) {
+		this.style = style;
+	}
+
+	public void setPaint(Paint paint) {
+		this.paint = paint;
 	}
 	
 }
--- src/lb/simplebase/javacore/scene/RangedDrawable.java
+++ src/lb/simplebase/javacore/scene/RangedDrawable.java
@@ -2,19 +2,47 @@ package lb.simplebase.javacore.scene;
 
 import java.awt.Graphics2D;
 
+/**
+ * A {@link RangedDrawable} represents an object of large or possibly infinite size that has a graphical representation.
+ * The {@link #draw(Graphics2D, int, int, double, double, double, double)} method is used to draw a finite section of this object.
+ * However, {@link RangedDrawable}s are not meant to be drawn directly by user code, but by other objects that manage the transformations.
+ */
 public interface RangedDrawable {
 	
-	public void draw(Graphics2D g2d, int width, int height, double minXunits, double minYunits, double maxXunits, double maxYunits);
+	/**
+	 * Draws a section of this {@link RangedDrawable}. It is usually valid to assume that the origin is centered on the screen an the y-axis is pointing upwards.
+	 * @param g2d The used graphics context. If transforms are applied, they must be reversed before thd method returns. It may not be disposed.
+	 * @param width The width of the area to draw in, in pixels
+	 * @param height The height of the area to draw in, in pixels
+	 * @param originXOffset The amount of logical units that the origin's x coordinate is offset from the center of the screen. A positive offset means a movement of the origin to the right
+	 * @param originYOffset The amount of logical units that the origin's y coordinate is offset from the center of the screen. A positive offset means a movement of the origin to the top
+	 * @param spanXunits The amount of logical units that the drawn area should be wide 
+	 * @param spanYunits The amount of logical units that the drawn area should be high
+	 */
+	public void draw(Graphics2D g2d, int width, int height, double originXOffset, double originYOffset, double spanXunits, double spanYunits);
 	
+	/**
+	 * Creates a {@link RangedDrawable} that draws two other RangedDrawables in order, in the same range.
+	 * @param top The RangedDrawable to draw on top of the other
+	 * @param bottom  The RangedDrawable to draw below the other
+	 * @return A new {@link RangedDrawable} that draws two others
+	 */
 	public static RangedDrawable combine(RangedDrawable top, RangedDrawable bottom) {
-		return (g2d, width, height, minXunits, minYunits, maxXunits, maxYunits) -> {
+		return (g2d, width, height, minXunits, minYunits, maxXunits, maxYunits) -> { //The parameter names are not changed, but they work
 			bottom.draw(g2d, width, height, minXunits, minYunits, maxXunits, maxYunits);
 			top.draw(g2d, width, height, minXunits, minYunits, maxXunits, maxYunits);
 		};
 	}
 	
+	/**
+	 * Creates a {@link RangedDrawable} that draws three other RangedDrawables in order, in the same range.
+	 * @param top The RangedDrawable to draw on top of the other
+	 * @param middle The RangedDrawable to draw between the others
+	 * @param bottom  The RangedDrawable to draw below the other
+	 * @return A new {@link RangedDrawable} that draws two others
+	 */
 	public static RangedDrawable combine(RangedDrawable top, RangedDrawable middle, RangedDrawable bottom) {
-		return (g2d, width, height, minXunits, minYunits, maxXunits, maxYunits) -> {
+		return (g2d, width, height, minXunits, minYunits, maxXunits, maxYunits) -> { //The parameter names are not changed, but they work
 			bottom.draw(g2d, width, height, minXunits, minYunits, maxXunits, maxYunits);
 			middle.draw(g2d, width, height, minXunits, minYunits, maxXunits, maxYunits);
 			top.draw(g2d, width, height, minXunits, minYunits, maxXunits, maxYunits);
