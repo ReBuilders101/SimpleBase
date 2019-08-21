5e8a038a64d4d72ca61816f980797331e80f13b3
ReBuilders101 <rebuilders101@gmail.com>
2019-08-21 12:37:23 +0200

    Removed unnecessary class

--- src/lb/simplebase/net/InboundPacketThreadHandler.java
+++ src/lb/simplebase/net/InboundPacketThreadHandler.java
@@ -3,8 +3,9 @@ package lb.simplebase.net;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.RejectedExecutionException;
-import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.atomic.AtomicInteger;
+
+import lb.simplebase.util.NamedThreadFactory;
 
 public class InboundPacketThreadHandler implements PacketReceiver{
 
@@ -12,10 +13,10 @@ public class InboundPacketThreadHandler implements PacketReceiver{
 	
 	private final PacketReceiver delegateThreadReceiver;
 	private final ExecutorService threadExecutor;
-	private final ThreadNameFactory fac;
+	private final NamedThreadFactory fac;
 	
 	public InboundPacketThreadHandler(PacketReceiver delegate, int threadCount) {
-		fac = new ThreadNameFactory(ibhID.getAndIncrement());
+		fac = new NamedThreadFactory("InboundPacketThreadHandler-" + ibhID.getAndIncrement() + "-Thread-");
 		delegateThreadReceiver = delegate;
 		if(threadCount <= 0) {
 			threadExecutor = Executors.newCachedThreadPool(fac);
@@ -31,23 +32,6 @@ public class InboundPacketThreadHandler implements PacketReceiver{
 		} catch (RejectedExecutionException e) {
 			NetworkManager.NET_LOG.warn("Rejected Packet handler execution: Service might be shut down already - Packet dropped", e);
 		}
-	}
-	
-	public static class ThreadNameFactory implements ThreadFactory{
-		
-		private final AtomicInteger threadId = new AtomicInteger(0);
-		private final int handlerId;
-		
-		public ThreadNameFactory(int hid) {
-			handlerId = hid;
-		}
-		
-		@Override
-		public Thread newThread(Runnable var1) {
-			Thread t = new Thread(var1);
-			t.setName("InboundPacketThreadHandler-" + handlerId + "-Thread-" + threadId.getAndIncrement());
-			return t;
-		}	
 	}
 	
 	public void shutdownExecutor() {
