5e96711001a7403654209187654a86b9dfbf3c64
ReBuilders101 <rebuilders101@gmail.com>
2019-09-10 18:55:31 +0200

    Added method with primitives

--- src/lb/simplebase/util/RenderDelegate.java
+++ src/lb/simplebase/util/RenderDelegate.java
@@ -5,4 +5,11 @@ import java.awt.Graphics2D;
 @FunctionalInterface
 public interface RenderDelegate extends TriConsumer<Graphics2D, Integer, Integer>{
 
+	@Override
+	public default void accept(Graphics2D t, Integer s, Integer u) {
+		draw(t, s, u);
+	}
+
+	public void draw(Graphics2D t, int s, int u);
+	
 }
