4360bedae1e6e8848382acb7f3a0f1e1258fe2e9
ReBuilders101 <rebuilders101@gmail.com>
2019-09-18 18:09:48 +0200

    Added method to create custom Ids

--- src/lb/simplebase/net/RemoteIDGenerator.java
+++ src/lb/simplebase/net/RemoteIDGenerator.java
@@ -2,18 +2,28 @@ package lb.simplebase.net;
 
 import java.net.InetSocketAddress;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
-public class RemoteIDGenerator implements Function<InetSocketAddress, TargetIdentifier>{
+public final class RemoteIDGenerator implements Function<InetSocketAddress, TargetIdentifier>{
 	
 	private static final AtomicInteger counter = new AtomicInteger(0);
+	private static BiFunction<InetSocketAddress, Integer, TargetIdentifier> genFunc = null;
 	
-	public static TargetIdentifier generateID(InetSocketAddress address) {
-		return TargetIdentifier.createNetwork("RemoteTarget-" + counter.getAndIncrement(), address);
+	public static TargetIdentifier generateID(final InetSocketAddress address) {
+		if(genFunc == null) {
+			return TargetIdentifier.createNetwork("RemoteTarget-" + counter.getAndIncrement(), address);
+		} else {
+			return genFunc.apply(address, counter.getAndIncrement());
+		}
 	}
 
+	public static void setGeneratorFunction(final BiFunction<InetSocketAddress, Integer, TargetIdentifier> func) {
+		genFunc = func;
+	}
+	
 	@Override
-	public TargetIdentifier apply(InetSocketAddress address) {
+	public TargetIdentifier apply(final InetSocketAddress address) {
 		return generateID(address);
 	}
 	
